8251cd23eb98b101f45c8e19e286f67532d084e5
vin-ce <yc.li.vincent@gmail.com>
2020-12-21 19:12:31 +1300

    template tweak

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
