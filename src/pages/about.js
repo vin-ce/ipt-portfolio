6a23d88e90b75d0117d2a67d57de5e72222c6220
vin-ce <yc.li.vincent@gmail.com>
2020-12-21 19:36:57 +1300

    config tweak

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
-import data from "../../sections/about.json"
+
 
 const About = props => {
   return (
     <Layout>
       <div>About</div>
-      <div>{ data.description }</div>
     </Layout>
   )
 }
