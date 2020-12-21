d089a99f468f34247d6ebb86c100392029a11983
vin-ce <yc.li.vincent@gmail.com>
2020-12-21 19:01:40 +1300

    re-add layout

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+// add sth to do with Helmet here
+const Layout = props => {
+  return (
+    <div>
+      Layout
+      {props.children }
+    </div>
+  )
+}
+
+export default Layout
